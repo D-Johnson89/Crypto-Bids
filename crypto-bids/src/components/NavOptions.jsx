b56b0d311bd3d79c7202787b0326de25aeba56f6
Dustin <d.l.johnson3009@gmail.com>
2022-11-25 11:31:23 -0500

    Removed useAuthUser from NavOptions(not used)

--- crypto-bids/src/components/NavOptions.jsx
+++ crypto-bids/src/components/NavOptions.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import LogOptions from './LogOptions'
 import Dashboard from './Dashboard'
 import { Outlet } from 'react-router-dom'
-import { useIsAuthenticated, useAuthUser } from 'react-auth-kit'
+import { useIsAuthenticated} from 'react-auth-kit'
 
 
 // Function to check if Logged In, Determines Nav Option to use
 function NavOptions() {
     const isAuthenticated = useIsAuthenticated()
-    const auth = useAuthUser()
 	
     
     return (
