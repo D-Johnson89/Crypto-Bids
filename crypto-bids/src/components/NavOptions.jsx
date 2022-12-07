77bb337e39bac95cce427ad2ef864cf4d56d483b
Dustin <d.l.johnson3009@gmail.com>
2022-12-07 14:34:28 -0500

    push to switch branches was following a suggestion but may have been unneeded, want to try something with react-auth-kits provided context again

--- crypto-bids/src/components/NavOptions.jsx
+++ crypto-bids/src/components/NavOptions.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LogOptions from './LogOptions'
 import Dashboard from './Dashboard'
 import { Outlet } from 'react-router-dom'
 import { useIsAuthenticated } from 'react-auth-kit'
-import { UserContextProvider } from '../util/UserContext'
+import { UserContextProvider, UserContext } from '../util/UserContext'
 
 
 
 // Function to check if Logged In, Determines Nav Option to use
 function NavOptions() {
     const isAuthenticated = useIsAuthenticated()
+    const [user, setUser] = useState()
 	
     
     return (
