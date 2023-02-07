178928dd3f7f18fd5c2562011d3a20e43360394b
Dustin <d.l.johnson3009@gmail.com>
2023-02-07 10:46:00 -0500

    Fix Typo
    Inserted spaces at beginning and end of environment property on line 30 for consistency.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -27,7 +27,7 @@ const Transactions = new mongoose.Schema(
 
 const User = new mongoose.Schema(
 	{
-        environment: {type: String, default: 'practice'},
+        environment: { type: String, default: 'practice' },
 		username: { type: String, required: true, unique: true },
 		email: { type: String, required: true, unique: true },
 		hash: { type: String, required: true },
